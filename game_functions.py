b63a3808b785c140866c726fcdcef057ad9655fe
hte202 <hte202@exeter.ac.uk>
2026-01-18 14:17:04 +0000

    game_function update

--- game_functions.py
+++ game_functions.py
@@ -9,10 +9,11 @@ def pick_value(poss_values):
 def check_higher_lower(current_val, next_val, user_input):
     if (current_val > next_val) and (user_input == "l"):
         return True
-    elif (current val < next_val) and (user_input == "h"):
+    elif (current_val < next_val) and (user_input == "h"):
         return True
     else:
         return False
+    pass
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
